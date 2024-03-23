905551710fcc658e46d6cbedf21fc54498fb39c1
rail <rail.shabayev@mail.ru>
2024-03-23 09:25:12 +0400

    feat: add Second exercise

---
+++ src/main/java/org/rail/Second.java
@@ -0,0 +1,39 @@
+package org.rail;
+
+public class Second {
+    public static void main(String[] args) {
+        new Thread(new Fibonacci(18)).start();
+        new Thread(new Fibonacci(6)).start();
+        new Thread(new Fibonacci(8)).start();
+    }
+    public interface Generator<T> {
+        T next();
+    }
+
+    static class Fibonacci implements Generator<Integer>, Runnable {
+        int n;
+        private int count = 0;
+
+        public Fibonacci(int n) {
+            this.n = n;
+        }
+
+        private int fib(int x) {
+            if (x < 2) return 1;
+            return fib(x - 2) + fib(x - 1);
+        }
+
+        @Override
+        public Integer next() {
+            return fib(count++);
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < n ; i++) {
+                System.out.println(Thread.currentThread().getName() + " " + next() + " ");
+            }
+            System.out.println("Fibonacci finished - " + Thread.currentThread().getName());
+        }
+    }
+}
