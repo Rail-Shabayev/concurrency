c9e6d80a26afcc223cbb5d7749206ee671a9f5b0
rail <rail.shabayev@mail.ru>
2024-03-23 09:43:53 +0400

    feat: add Third exercise

---
+++ src/main/java/org/rail/Third.java
@@ -0,0 +1,37 @@
+package org.rail;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Third {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newFixedThreadPool(3);
+        exec.submit(new SuperClass("started", "ended", 1));
+        exec.submit(new SuperClass("started", "ended", 2));
+        exec.submit(new SuperClass("started", "ended", 3));
+        exec.shutdown();
+    }
+
+    static class SuperClass implements Runnable {
+        String start;
+        String shutdown;
+        int id;
+
+        public SuperClass(String start, String shutdown, int id) {
+            this.start = start;
+            this.shutdown = shutdown;
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(id + " " + start);
+            for (int i = 0; i < 3; i++) {
+                System.out.println(id + " some message " + i);
+                Thread.yield();
+            }
+            System.out.println(id + " " + shutdown);
+        }
+    }
+}
+
